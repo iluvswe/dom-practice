481791d9feee7d7ffda52776775349b15fa348b3
iluvswe <swe01swe@gmail.com>
2022-09-12 19:02:26 -0400

    my original hw answers

---
+++ scriptpractice.js
@@ -0,0 +1,48 @@
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+for (link of menuLinks) {
+    let newAnchor = document.createElement("a");
+    newAnchor.setAttribute("href", link.href);
+    newAnchor.textContent = link.text;
+    topMenuEl.appendChild(newAnchor); }
+  
+const subMenuEl = document.getElementbyId("#sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+// Menu data structure
+var menuLinks = [
+    {text: "about", href: "/about"},
+    {text: "catalog", href: "#", subLinks: [
+      {text: "all", href: "/catalog/all"},
+      {text: "top selling", href: "/catalog/top"},
+      {text: "search", href: "/catalog/search"},
+    ]},
+    {text: "orders", href: "#" , subLinks: [
+      {text: "new", href: "/orders/new"},
+      {text: "pending", href: "/orders/pending"},
+      {text: "history", href: "/orders/history"},
+    ]},
+    {text: "account", href: "#", subLinks: [
+      {text: "profile", href: "/account/profile"},
+      {text: "sign out", href: "/account/signout"},]},
+    ];
+  
+const topMenuLinks = document.querySelectorAll("#top-menu");
+let showingSubMenu = false;
+  
+topMenuEl.addEventListener("click", (e) => {e.preventDefault();
+showingSubMenu = false;
+subMenuEl.style.top = "0";
+    
+
